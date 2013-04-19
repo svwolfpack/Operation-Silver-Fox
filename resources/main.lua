840ce741aee1ddc4de9bceee551df9a70178818d
Sam Voigt <sam@intrepid.io>
2013-04-19 17:03:46 -0400

    renamed level screen file

--- resources/main.lua
+++ resources/main.lua
@@ -8,7 +8,7 @@
 
 local homeScreen = dofile("home.lua")
 local levelSelect = dofile("levelSelect.lua")
-local mainLevelScreen = dofile("mainLevelScreen.lua")
+local levelScreen = dofile("levelScreen.lua")
 
 local transitionHandler = function(event)
 	event.transitionType = event.transitionType or "slideInL"
@@ -18,8 +18,8 @@ local transitionHandler = function(event)
 	elseif event.screen == "home" then
 		director:moveToScene(homeScreen, {transitionType=event.transitionType, transitionTime=0.5})
   elseif event.screen == "main level screen" then
-    mainLevelScreen.levelFileName = event.levelFileName
-    director:moveToScene(mainLevelScreen, {transitionType=event.transitionType, transitionTime=0.5})
+    levelScreen.levelFileName = event.levelFileName
+    director:moveToScene(levelScreen, {transitionType=event.transitionType, transitionTime=0.5})
 	end
 end
 
