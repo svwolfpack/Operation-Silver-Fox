1fcfc4105256c18b9ab10de4fe0f102ab22741c7
Sam Voigt <sam@intrepid.io>
2013-05-02 17:53:23 -0400

    slight rest action refactor

--- resources/actionRest.lua
+++ resources/actionRest.lua
@@ -13,24 +13,15 @@ function actionRest:beat()
   if self.beatCount == 1 then
     self.block.isMoving = true
     self.block = nil
-    self.beatCount = 0
- else
+  else
     self.beatCount = self.beatCount + 1
-  end
- 
-  --self.block = nil
-  --if self.block.beatCount >= self.duration
-end
-
-function actionRest:update(event)
-  self.elapsedBeatTime = self.elapsedBeatTime + system.deltaTime
-  if self.elapsedBeatTime >= self.secondsPerBeat then
-    --self:releaseBlock()
-    system:removeEventListener("update", self)
   end
 end
 
 function actionRest:centerCollisionWithItem(item)
+  if item ~= self.block then
+    self.beatCount = 0
+  end
   self.block = item
   self.block.isMoving = false
   return nil
@@ -45,12 +36,9 @@ end
 function actionRest:init(a, itemData)
   itemData.color = {0, 150, 255}
   cTouchItem:init(a, itemData)
-  a.duration = itemData.duration or 1
   a.secondsPerBeat = itemData.secondsPerBeat or 1
   a.beatCount = 0
-  a.elapsedBeatTime = 0
   a.block = nil
-
 end
 
 return actionRest
